b39bc5e4fe7320c1699eb19604f9d01ba96aac71
Ilyas <ilyas.ilyasov.96@yandex.ru>
2020-10-15 10:42:26 +0300

    Rolling before show

--- dice.rb
+++ dice.rb
@@ -1,7 +1,16 @@
-puts "How many dice?"
 
+def roll_the_dice
+  100.times do
+    print "#{rand(6) + 1}\r"
+    sleep 0.01
+  end
+end
+
+puts "How many dice?"
 num = gets.chomp.to_i
+
+roll_the_dice
+puts "Кубики выпали!"
 num.times do
   puts rand(6) + 1
 end
-#end
